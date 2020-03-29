76c8d17c07a9ad7ce200fa05492a99516e369a17
rixong <rixong@gmail.com>
2020-03-29 08:48:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    console.log('meow!');
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    console.log('woof!');
+    
+  }
+
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    if (this.sex === 'male') {
+      console.log(`It's me! ${this.name}`);
+    } else {
+      console.log('squwak!');
+    }
+  }
+}
